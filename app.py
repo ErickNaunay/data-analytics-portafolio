dd17eec9efb9d258c22cd59fdb862ac5fdb159da
en-tripleten <enaunay@gmail.com>
2024-11-30 13:26:14 -0500

    feature: analisis exploratiorio compuesto con UI

--- app.py
+++ app.py
@@ -4,25 +4,80 @@ import sidetable as stb
 import plotly.express as px
 
 st.title("_DA Portafolio_ de :blue[Erick Naunay] :sunglasses:")
-st.write('Analisis exploratorio inicial:')
+st.header('Analisis exploratorio inicial:')
 
 raw_data = pd.read_csv('datasets/movie_metadata.csv')
 
-st.write('Revisar primeras filas:')
+st.subheader('Total de filas en el dataframe:')
+st.write(len(raw_data))
+
+st.subheader('Tipos de datos de las columnas:')
+st.dataframe(raw_data.dtypes.rename('datatype'))
+
+st.subheader('Revisar primeras filas:')
 st.dataframe(raw_data.head())
 
-st.write('Revisar primeras valores ausentes:')
+st.subheader('Revisar ultimas filas:')
+st.dataframe(raw_data.tail())
+
+st.subheader('Revisar valores ausentes:')
 st.dataframe(raw_data.stb.missing(style=True))
 
-boton_barras = st.button('Crear grafico de barras sobre puntaje de imdb', type='primary')
+st.subheader('Revisar filas duplicados:')
+st.write('El numero de filas duplicadas es: {}'.format(raw_data.duplicated().sum()))
 
-if boton_barras:
-    st.bar_chart(raw_data['imdb_score'])
+st.subheader('Estadisticas descriptivas de los datos numericos:')
+st.dataframe(raw_data.describe())
 
-boton_histograma= st.button('Crear grafico de histograma sobre puntaje de imdb')
+st.header('Limpieza y curacion de los datos')
 
-if boton_histograma:
-    fig = px.histogram(raw_data, x='imdb_score')
+with st.echo():
 
-    st.plotly_chart(fig)
+    clean_data = raw_data.drop_duplicates()
+
+    clean_data['gross'].interpolate(method='polynomial', order=5, inplace=True)
+
+    clean_data.dropna(inplace=True)
+
+    clean_data.drop('color', axis=1, inplace=True)
+
+    clean_data.reset_index(drop=False, inplace=True)
+
+st.subheader('Total de filas en el dataframe:')
+st.write(len(clean_data))
+
+st.subheader('Revisar valores ausentes:')
+st.dataframe(clean_data.stb.missing(style=True))
+
+st.subheader('Revisar filas duplicados:')
+st.write('El numero de filas duplicadas es: {}'.format(clean_data.duplicated().sum()))
+
+st.header('Analisis Exploratorio')
+
+dimension_selection = st.selectbox(
+    'Selecciona la dimension a comparar con el GROSS',
+    ('cast_total_facebook_likes', 'imdb_score', 'movie_facebook_likes')
+)
+
+#st.write('El usurio selecciono: {}'.format(dimension_selection))
+
+st.scatter_chart(clean_data, x='gross', y=dimension_selection)
+
+ventas_por_categoria = {}
+
+for index, fila in clean_data.iterrows():
+    
+    for genero in fila['genres'].split('|'):
+        
+        genero_limpio = genero.strip()
+
+        if genero_limpio not in ventas_por_categoria:
+            ventas_por_categoria[genero_limpio] = 0
+        
+        ventas_por_categoria[genero_limpio] += fila['gross']
+
+ventas_por_categoria_df = pd.DataFrame(list(ventas_por_categoria.items()), columns=['genero', 'gross'])
+
+st.dataframe(ventas_por_categoria_df)
+
 
